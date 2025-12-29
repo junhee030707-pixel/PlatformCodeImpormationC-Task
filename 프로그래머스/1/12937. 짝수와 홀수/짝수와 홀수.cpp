3c28b7b81b71b0201cbe83043750106ba4a2edc2
junhee030707-pixel <junhee030707@gmail.com>
2025-12-30 02:49:47 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num) {
+    string answer = "Even";
+    string answer2 = "Odd";
+    if(num % 2 ==0)
+    {
+        return answer;
+    }
+    else 
+    {
+    return answer2;
+    }
+}
